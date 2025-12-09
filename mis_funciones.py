d057deaa9f30bb3efd0fc690a051eb8af276dea6
Daniel <dan.cbaez@gmail.com>
2025-12-09 13:18:27 -0300

    comentarios

--- mis_funciones.py
+++ mis_funciones.py
@@ -1,3 +1,6 @@
+# Este es un módulo que contiene 2 funciones. Entonces puedes importar este modulo desde 
+# un archivo .py y así podras utilizar las funciones existentes en este modulo.
+
 def saludar(nombre):
     return f"Hola {nombre}, éxito en el examen!"
 
