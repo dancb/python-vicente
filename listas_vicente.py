8dcfbdb7f34e9cd45367629290950433218273ba
Daniel <dan.cbaez@gmail.com>
2025-12-09 12:40:08 -0300

    listas y matriz

---
+++ listas_vicente.py
@@ -0,0 +1,30 @@
+# definimos la lista con la que vamos a trabajar
+lista = [1, 2, "hola", 3, "chao", 4, 5]
+print(lista)
+
+print(" ")
+print(" ")
+
+# Itero de una forma simple a través de la lista
+# la variable 'elemento' al es la que recorre cada valor en la lista, entonces la puedo imprimir 
+# y saber su valor en cada iteración
+for elemento in lista:
+    print(elemento)
+    
+print(" ")
+print(" ")
+
+# Aqui utilizo la funcion range dandole 2 argumentos, el 'desde' que es 1 y el 'hasta' que es 4.
+# Dado que las listas tienen indices que posicionan cada elemento con un numero desde 0 al n, en este caso
+# si le digo que quiero desde el elemento 1 (indice 0) hasta el 4 (indice 3), me va a imprimir los elementos de la posición 1 (que es el valor 2)
+# y tambien el elemento de la pisición 4-1 (por que 4-1? Porque la funcion de python range para el argumento 'hasta', no es inclusivo, 
+# es decir si le dices que quieres el elemento 4, es como que le digas que quieres el elemento 3 (4-1))
+for i in range(1, 4):
+    print(lista[i])
+    
+print(" ")
+print(" ")
+
+# La funcion len (que significa longitud en inglés),
+# permite saber la longitud de la lista (cuenta los elementos existentes en la lista, partiendo desde el 1)
+print(f"La logitud es la lista es: {len(lista)}")
---
+++ matriz_vicente.py
@@ -0,0 +1,33 @@
+# Aqui una matriz que está compuesta por listas (lista de listas)
+matriz = [
+    [1, 2, 3],
+    ["hola", "hello", "ciao"],
+    [True, False, True]
+]
+
+# El primer 'for' elige la fila completa
+# El segundo 'for' corresponde a la columna 
+# y el cruce de la fila con la columna te da la 'celda' (si lo imaginamos con una hoja de excel)
+for fila in matriz:
+    for columna in fila:
+        print(f"El valor en la celda actual es: {columna}")
+
+print(" ")
+print(" ")
+
+for x in range(len(matriz)):
+    for y in range(len(matriz[x])):
+        print(f"El valor en la celda actual es: {matriz[x][y]}")
+        print(f"El valor del eje x es: {matriz[x]}")
+        print(f"El valor del eje y es: {matriz[y]}")
+        
+print(" ")
+print(" ")
+
+for x in range(len(matriz)):
+    for y in range(len(matriz[x])):
+        print(f"El valor en la celda actual es: {matriz[x][y]}")
+        print(f"El valor del eje x es: {matriz[x]}")
+        print(" ")
+
+
